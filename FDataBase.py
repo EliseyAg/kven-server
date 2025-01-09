a425bd8cc057ff4fc8913e51e6d8099e66c70962
EliseyAg <elisey.tagan@gmail.com>
2025-01-09 18:06:24 +0300

    init commit

---
+++ FDataBase.py
@@ -0,0 +1,48 @@
+import math
+import sqlite3
+import time
+
+
+class FDataBase:
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def addUser(self, username,  hashpass):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?)", (username, hashpass, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления в БД: " + str(e))
+            return False
+
+        return True
+
+    def getUserById(self, user_id):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE id = '{user_id}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД " + str(e))
+
+        return False
+
+    def getUserByName(self, user_name):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE name = '{user_name}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД " + str(e))
+
+        return False
---
+++ main.py
@@ -0,0 +1,126 @@
+from flask import Flask, flash, render_template, request, redirect, g
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+import os
+
+from FDataBase import FDataBase
+from UserLogin import UserLogin
+
+
+DATABASE = '/tmp/users.bd'
+DEBUG = True
+SECRET_KEY = 'jgfjlfkj765@68976,<34'
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'users.db')))
+
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print("load user")
+    return UserLogin().fromDB(user_id, dbase)
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+dbase = None
+
+
+@app.before_request
+def before_request():
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['POST', "GET"])
+def login():
+    if request.method == "POST":
+        user = dbase.getUserByName(request.form['username'])
+        if user and check_password_hash(user['psw'], request.form['password']):
+            userlogin = UserLogin().create(user)
+            login_user(userlogin)
+            return redirect("/profile")
+
+    return render_template("login.html")
+
+
+@app.route('/register', methods=['POST', "GET"])
+def register():
+    if request.method == "POST":
+        if len(request.form['username']) > 4 and request.form['password'] == request.form['password2']:
+            hash = generate_password_hash(request.form['password'])
+            res = dbase.addUser(request.form['username'], hash)
+            if res:
+                flash("Вы успешно зарегистрированы", "success")
+                return redirect("/profile")
+            else:
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+            flash("Неверно заполнены поля", "error")
+
+    return render_template("register.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect("/login")
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return f"""<p><a href="/logout">Выйти из профиля</a>
+               <p>user info: {current_user.get_id()}"""
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page: " + name + " - " + str(id)
+
+
+if __name__ == "__main__":
+    app.run()
