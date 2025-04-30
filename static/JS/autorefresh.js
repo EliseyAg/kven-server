aed2e1f5da8bc82648e26c71a6a165b62876a286
EliseyAg <elisey.tagan@gmail.com>
2025-04-30 16:33:10 +0300

    Updated scripts

--- static/JS/autorefresh.js
+++ static/JS/autorefresh.js
@@ -1,8 +1,10 @@
 function refresh()
 {
-    var iframe = document.getElementById('FrameID');
-    iframe.contentWindow.location.reload();
+    //var iframe = document.getElementById('FrameID');
+    //iframe.contentWindow.location.reload();
     //iframe.src = iframe.src;
+    var loc = window.location;
+    window.location = loc.protocol + '//' + loc.host + loc.pathname + loc.search;
 }
 
-window.setInterval(refresh, 2000);
+//window.setInterval(refresh, 20000);
